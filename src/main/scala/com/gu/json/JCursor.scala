f55ec45a1b69008c50871efedd9772cd4846169b
Ben James <ben.james@guardian.co.uk>
2013-04-12 16:45:37 +0100

    Documentation for JCursor methods

--- src/main/scala/com/gu/json/JCursor.scala
+++ src/main/scala/com/gu/json/JCursor.scala
@@ -12,34 +12,41 @@ import JCursor._
   */
 final case class JCursor(focus: JValue, path: Path) {
 
+  /** Replace the value at the focus */
   def replace(newFocus: JValue): JCursor =
     copy(focus = newFocus)
 
+  /** Transform the value at the focus */
   def transform(f: PartialFunction[JValue, JValue]): JCursor =
     replace(focus.transform(f))
 
+  /** Transform the value at the focus */
   def withFocus(f: JValue => JValue): JCursor =
     replace(f(focus))
 
   def withFocusF[F[_] : Functor](f: JValue => F[JValue]): F[JCursor] =
     Functor[F].map(f(focus))(replace)
 
+  /** Delete the value at the focus, and move up one level */
   def deleteGoUp: Option[JCursor] =
     condOpt(path) {
       case InArray(lefts, rights) :: p => JCursor(JArray(lefts reverse_::: rights), p)
       case InObject(_, lefts, rights) :: p => JCursor(JObject(lefts reverse_::: rights), p)
     }
 
+  /** Delete the array element at the focus, and move to the next element on the right */
   def deleteGoRight: Option[JCursor] =
     condOpt(path) {
       case InArray(ls, r::rs) :: p => JCursor(r, InArray(ls, rs) :: p)
     }
 
+  /** Insert a new array element at the left of the focus, and move focus to the new element */
   def insertLeft(newElem: JValue): Option[JCursor] =
     condOpt(path) {
       case InArray(ls, rs) :: p => JCursor(newElem, InArray(ls, focus::rs) :: p)
     }
 
+  /** Insert a new array element at the right of the focus, and move focus to the new element */
   def insertRight(newElem: JValue): Option[JCursor] =
     condOpt(path) {
       case InArray(ls, rs) :: p => JCursor(newElem, InArray(focus::ls, rs) :: p)
@@ -51,16 +58,19 @@ final case class JCursor(focus: JValue, path: Path) {
       case JArray(elems) => copy(focus = JArray(elem :: elems))
     }
 
+  /** Move the focus left by one array element */
   def left: Option[JCursor] =
     condOpt(path) {
       case InArray(l::ls, rs) :: p  => JCursor(l, InArray(ls, focus::rs) :: p)
     }
 
+  /** Move the focus right by one array element */
   def right: Option[JCursor] =
     condOpt(path) {
       case InArray(ls, r::rs) :: p => JCursor(r, InArray(focus::ls, rs) :: p)
     }
 
+  /** Find an array element to the left of the focus matching */
   def findLeft(pfn: PartialFunction[JValue, Boolean]): Option[JCursor] =
     path match {
       case InArray(ls, rs) :: path  => ls.span(l => ! cond(l)(pfn)) match {
@@ -70,17 +80,20 @@ final case class JCursor(focus: JValue, path: Path) {
       case _ => None
     }
 
+  /** Move the focus down to the first element of an array */
   def firstElem: Option[JCursor] =
     condOpt(focus) {
       case JArray(x::xs) => JCursor(x, InArray(Nil, xs) :: path)
     }
 
+  /** Move the focus down to the array element at the specified index */
   def elem(index: Int): Option[JCursor] =
     for {
       first <- firstElem
       elem  <- List.fill(index)(Kleisli((_: JCursor).right)).foldRight(Kleisli.ask[Option, JCursor])(_ <=< _).run(first)
     } yield elem
 
+  /** Move the focus up one level */
   def up: Option[JCursor] =
     condOpt(path) {
       case InArray(lefts, rights) :: p => JCursor(JArray(lefts reverse_::: (focus::rights)), p)
@@ -92,6 +105,7 @@ final case class JCursor(focus: JValue, path: Path) {
       case JObject(fields) => fields.map(_._1).toSet
     }
 
+  /** Move the focus to the named field in an object */
   def field(name: String): Option[JCursor] =
     focus match {
       case JObject(fields) => fields.span(_._1 != name) match {
@@ -101,32 +115,38 @@ final case class JCursor(focus: JValue, path: Path) {
       case _ => None
     }
 
+  /** Prepend a field to the object in the focus, and move the focus to the value of the new field */
   def insertField(name: String, value: JValue): Option[JCursor] =
     condOpt(focus) {
       case JObject(fields) => JCursor(value, InObject(name, Nil, fields) :: path)
     }
 
+  /** Move the focus to the named field, at the same level as the current focus in an object */
   def sibling(name: String): Option[JCursor] =
     path match {
       case InObject(_, _, _) :: _ => up flatMap (_.field(name))
       case _ => None
     }
 
+  /** Insert a new field at the left of the focus, and move focus to the new field value */
   def insertFieldLeft(name: String, value: JValue): Option[JCursor] =
     condOpt(path) {
       case InObject(n, ls, rs) :: p => JCursor(value, InObject(name, ls, JField(n, focus) :: rs) :: p)
     }
 
+  /** Insert a new field at the right of the focus, and move focus to the new field value */
   def insertFieldRight(name: String, value: JValue): Option[JCursor] =
     condOpt(path) {
       case InObject(n, ls, rs) :: p => JCursor(value, InObject(name, JField(n, focus) :: ls, rs) :: p)
     }
 
+  /** Rename the field at the focus */
   def rename(name: String): Option[JCursor] =
     condOpt(path) {
       case InObject(_, ls, rs) :: p => copy(path = InObject(name, ls, rs) :: p)
     }
 
+  /** Go back to the root of the tree */
   @tailrec
   def root: JCursor =
     up match {
@@ -134,6 +154,7 @@ final case class JCursor(focus: JValue, path: Path) {
       case None => this
     }
 
+  /** Retrieve the JValue at the root */
   def toJValue: JValue = root.focus
 
 }
