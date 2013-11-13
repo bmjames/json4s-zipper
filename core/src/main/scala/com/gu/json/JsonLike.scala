7640b883a3c4d7aa9126883773297cd1b6c81ac5
Ben James <ben.james@guardian.co.uk>
2013-11-13 17:57:02 +0000

    Convenience methods for polymorphic JSON construction

--- core/src/main/scala/com/gu/json/JsonLike.scala
+++ core/src/main/scala/com/gu/json/JsonLike.scala
@@ -7,7 +7,7 @@ trait JsonLike[J] {
   def array(elems: Seq[J]): J
   def obj(fields: Seq[(String, J)]): J
   def string(s: String): J
-  def int(s: BigInt): J
+  def int(i: BigInt): J
   def double(d: Double): J
   def bool(b: Boolean): J
 
@@ -18,4 +18,23 @@ trait JsonLike[J] {
   def asDouble(j: J): Option[Double]
   def asBool(j: J): Option[Boolean]
 
+}
+
+object JsonLike {
+  def apply[J](implicit ev: JsonLike[J]): JsonLike[J] = ev
+
+  def nothing[J : JsonLike]: J = JsonLike[J].nothing
+  def array[J : JsonLike](elems: Seq[J]): J = JsonLike[J].array(elems)
+  def obj[J : JsonLike](fields: Seq[(String, J)]): J = JsonLike[J].obj(fields)
+  def string[J : JsonLike](s: String): J = JsonLike[J].string(s)
+  def int[J : JsonLike](i: BigInt): J = JsonLike[J].int(i)
+  def double[J : JsonLike](d: Double): J = JsonLike[J].double(d)
+  def bool[J : JsonLike](b: Boolean): J = JsonLike[J].bool(b)
+
+  def asArray[J : JsonLike](j: J): Option[List[J]] = JsonLike[J].asArray(j)
+  def asObj[J : JsonLike](j: J): Option[List[(String, J)]] = JsonLike[J].asObj(j)
+  def asString[J : JsonLike](j: J): Option[String] = JsonLike[J].asString(j)
+  def asInt[J : JsonLike](j: J): Option[BigInt] = JsonLike[J].asInt(j)
+  def asDouble[J : JsonLike](j: J): Option[Double] = JsonLike[J].asDouble(j)
+  def asBool[J : JsonLike](j: J): Option[Boolean] = JsonLike[J].asBool(j)
 }
